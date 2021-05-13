f520f8219db76a567aeee2dd68f8c7e7767d9000
snaulX <alexandergunger666@gmail.com>
2021-05-14 00:55:21 +0600

    Create Enigma.Input C# project

---
+++ Enigma.Input/IInputable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+
+namespace Enigma.Input
+{
+    public interface IInputable
+    {
+        InputSnapshot Snapshot { protected set; get; }
+        Vector2 MouseDelta { protected set; get; }
+        bool MouseVisible { get; set; }
+
+        event Action<KeyEvent> OnKeyDown, OnKeyUp;
+        event Action<MouseEvent> OnMouseDown, OnMouseUp;
+        event Action<MouseMoveEventArgs> OnMouseMove;
+        event Action<MouseWheelEventArgs> OnMouseScroll;
+
+        void SetMousePosition(Vector2 position);
+    }
+}
---
+++ Enigma.Input/Input.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+
+namespace Enigma.Input
+{
+    public static class Input
+    {
+        public static Vector2 MouseDelta { get; }
+        public static bool MouseVisible { get; set; }
+
+        public static event Action<KeyEvent> OnKeyDown, OnKeyUp;
+        public static event Action<MouseEvent> OnMouseDown, OnMouseUp;
+        public static event Action<MouseMoveEventArgs> OnMouseMove;
+        public static event Action<MouseWheelEventArgs> OnMouseScroll;
+
+        private static readonly List<IInputable> inputables = new ();
+        private static readonly List<InputSnapshot> snapshots = new ();
+
+        public static void AddInputable(IInputable inputable)
+        {
+            inputables.Add(inputable);
+        }
+
+        public static void Update()
+        {
+            snapshots.Clear();
+
+            foreach (IInputable i in inputables)
+            {
+                snapshots.Add(i.Snapshot);
+            }
+        }
+
+        public static bool IsKeyDown(Key key)
+        {
+            foreach (InputSnapshot s in snapshots)
+            {
+                foreach (KeyEvent e in s.KeyEvents)
+                {
+                    if (e.Key == key)
+                        return e.Down;
+                }
+            }
+            return false;
+        }
+
+        public static void SetMousePosition(Vector2 position)
+        {
+            foreach (IInputable i in inputables)
+                i.SetMousePosition(position);
+        }
+
+        public static void SetMousePosition(int x, int y)
+        {
+            SetMousePosition(new Vector2(x, y));
+        }
+    }
+}
